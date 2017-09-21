53dd4ea02fa386981afc4289194801ee58b06a21
Peter Kong <bansuki@gmail.com>
2017-09-20 19:53:52 -0500

    draw one card and draw five

--- deck-o-cards.js
+++ deck-o-cards.js
@@ -1,27 +1,32 @@
 // 1. Make the function deck_o_cards assemble an array of cards using the provided suits and values arrays.
 // Each card in the deck should be an object formatted as: {suit: 'hearts', value: 'A'}
-function deck_o_cards() {
+function deck_o_cards(is_poker=false) {
   var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
   var suits = ['hearts', 'diamonds', 'clubs', 'spades'];
 
   var cards = []; // deck
   var shuffledCards = []; // deck shuffled
 
-  // Make 52 card objects and store them in the "cards" array
-  // Hint: use 2 for loops
+  values.forEach(function (valueItem) {
+    suits.forEach(function (suitItem) {
+      cards.push({ value: valueItem, suit: suitItem });
+    });
+  });
 
+  cards = shuffle(cards);
 
-  // 2. Shuffle the cards
-  // Hint: shuffle function is already defined below
+  let handSize = is_poker ? 5 : 1;
+  let drawnCards = [];
+  for (let i = 0; i < handSize; i++) {
+    drawnCards.push(cards.pop());
+  }
 
+  console.log(`\nThe deck has ${cards.length} cards`);
 
-  // Pull the top card from the newly shuffledCards
-
-
-  // 3. Print the results:
-  // "The deck has {n} cards"
-  // "The top card is the {value} of {suit}"
-
+  while(drawnCards.length !== 0) {
+    let drawnCard = drawnCards.pop();
+    console.log(`${is_poker ? '' : 'The top card is the '}${drawnCard.value} of ${drawnCard.suit}`);
+  }
 }
 
 
@@ -47,3 +52,7 @@ function shuffle(array) {
 
     return array;
 }
+
+deck_o_cards();
+
+deck_o_cards(true);
